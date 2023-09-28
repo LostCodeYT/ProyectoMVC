77b8640d5c40a5a63768fd58f12dee9e0d0ec78e
Luis <ldpumacondor@gmail.com>
2023-09-28 18:25:21 -0500

    V1 Proyecto MVC

--- src/main/java/com/LostCode/MVC/MvcApplication.java
+++ src/main/java/com/LostCode/MVC/MvcApplication.java
@@ -1,5 +1,6 @@
 package com.LostCode.MVC;
 
+import com.LostCode.MVC.Controller.TareaController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MvcApplication.class, args);
+
+				TareaController controlador = new TareaController();
+
+				controlador.agregarTarea("Hacer compras");
+				controlador.agregarTarea("Estudiar para el examen");
+				controlador.agregarTarea("Ejercicio matutino");
+				controlador.marcarTareaComoCompletada(0);
+				controlador.agregarTarea("Llevar el coche al taller");
 	}
 
-}
+	}
+
+
